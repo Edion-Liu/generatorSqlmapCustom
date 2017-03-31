c79d107b98b880a1b5ca417c3f768b876c61c882
Edion@Edion-PC <3292677449@qq.com>
2017-03-31 15:49:25 +0800

    mybatis 逆向工程

---
+++ src/com/eliteams/quick4j/web/dao/TReportMapper.java
@@ -0,0 +1,30 @@
+package com.eliteams.quick4j.web.dao;
+
+import com.eliteams.quick4j.web.model.TReport;
+import com.eliteams.quick4j.web.model.TReportExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TReportMapper {
+    int countByExample(TReportExample example);
+
+    int deleteByExample(TReportExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TReport record);
+
+    int insertSelective(TReport record);
+
+    List<TReport> selectByExample(TReportExample example);
+
+    TReport selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") TReport record, @Param("example") TReportExample example);
+
+    int updateByExample(@Param("record") TReport record, @Param("example") TReportExample example);
+
+    int updateByPrimaryKeySelective(TReport record);
+
+    int updateByPrimaryKey(TReport record);
+}
